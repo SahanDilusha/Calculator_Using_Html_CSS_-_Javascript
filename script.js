fec99d52c30f186608ad6feb4984ce1003698a18
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-04 16:43:16 +0530

    "Updated index.html, script.js, and style.css files with minor changes to HTML structure, JavaScript logic, and CSS styling."

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
     item.onclick = () => {
-      if (item.id == "clear") {
-        display.innerHTML = "";
-      } else if (item.id == "backspase") {
-        let string = display.innerHTML.toString();
-        display.innerHTML = string.substr(0, string.length - 1);
-      } else if (display.innerHTML != "" && item.id == "equal") {
-        display.innerHTML = eval(display.innerHTML);
-      } else if (display.innerHTML == "" && item.id == "equal") {
-        display.innerHTML = "Empty!";
-        setTimeout(() => (display.innerHTML = ""), 2000);
-      } else {
-        display.innerHTML += item.id;
-      }
+        if (item.id == "clear") {
+            display.innerHTML = "";
+        } else if (item.id == "backspase") {
+            let string = display.innerHTML.toString();
+            display.innerHTML = string.substr(0, string.length - 1);
+        } else if (display.innerHTML != "" && item.id == "equal") {
+            display.innerHTML = eval(display.innerHTML);
+        } else if (display.innerHTML == "" && item.id == "equal") {
+            display.innerHTML = "Empty!";
+            setTimeout(() => (display.innerHTML = ""), 2000);
+        } else {
+            display.innerHTML += item.id;
+        }
     };
-  });
+});
 
 const themeToggle = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
