08d0615aa1eaa70681484a860b2e4399ed850b46
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-04 16:35:12 +0530

    "Updated index.html to add IDs to number buttons and modified script.js to handle button clicks and add theme toggling functionality."

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
     item.onclick = () => {
+      if (item.id == "clear") {
+        display.innerHTML = "";
+      } else if (item.id == "backspace") {
+        let string = display.innerHTML.toString();
+        display.innerHTML = string.substr(0, string.length - 1);
+      } else if (display.innerHTML != "" && item.id == "equal") {
+        display.innerHTML = eval(display.innerHTML);
+      } else if (display.innerHTML == "" && item.id == "equal") {
+        display.innerHTML = "Empty!";
+        setTimeout(() => (display.innerHTML = ""), 2000);
+      } else {
+        display.innerHTML += item.id;
+      }
+    };
+  });
 
-        if (item.id === "clear") {
-            display.innerHTML = "";
-        } else if (item.id == "backspase") {
-            let text = display.innerHTML.toString();
-            display.innerHTML = text.substring(0, text.length - 1);
-        } else if (display.innerHTML != '' && item.id == 'equal') {
+const themeToggle = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const togglerIcon = document.querySelector(".toggler-icon");
+let isDark = true;
 
-        }
-
-
-    }
-})
+themeToggle.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggle.classList.toggle("active");
+    isDark = !isDark;
+}
